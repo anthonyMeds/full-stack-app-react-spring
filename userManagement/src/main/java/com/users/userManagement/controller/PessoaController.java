d3cb6ea569a2f0c95c3452da1a10f1aac14716c6
anthonyMeds <anthony.eduardo@hotmail.com>
2024-12-08 03:00:58 -0300

    docs: Swagger docs

--- userManagement/src/main/java/com/users/userManagement/controller/PessoaController.java
+++ userManagement/src/main/java/com/users/userManagement/controller/PessoaController.java
@@ -5,6 +5,7 @@ import com.users.userManagement.DTO.pessoa.PessoaRequestDTO;
 import com.users.userManagement.DTO.pessoa.PessoaResponseDTO;
 import com.users.userManagement.config.exception.ServiceException;
 import com.users.userManagement.domain.service.PessoaService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +28,14 @@ public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
+    @Operation(summary = "Cadastrar Usuário", description = "Efetua o cadastro de um novo usuário.")
     @PostMapping()
     public ResponseEntity<PessoaResponseDTO> cadastrarPessoa(@Valid @RequestBody PessoaRequestDTO pessoaRequestDTO) throws ServiceException {
         PessoaResponseDTO response = pessoaService.cadastrarPessoa(pessoaRequestDTO);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Atualizar Usuário", description = "Atualiza um usuário pelo id.")
     @PutMapping("/{id}")
     public ResponseEntity<PessoaResponseDTO> atualizarUsuario(@Valid @RequestBody PessoaRequestDTO pessoaRequestDTO,
                                                               @PathVariable @NotNull Integer id) throws ServiceException {
@@ -40,18 +43,21 @@ public class PessoaController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Detalhar Usuário", description = "Obtém os detalhes de um usuário pelo id.")
     @GetMapping("/{id}")
     public ResponseEntity<DetalhePessoaResponseDTO> buscarDetalheUsuario(@PathVariable @NotNull Integer id) throws ServiceException {
         DetalhePessoaResponseDTO response = pessoaService.buscarDetalheUsuario(id);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Deletar Usuário", description = "Deleta um usuário pelo id.")
     @DeleteMapping("/{id}")
     public ResponseEntity<PessoaResponseDTO> deletarUsuario(@PathVariable @NotNull Integer id) throws ServiceException {
         PessoaResponseDTO response = pessoaService.deletarUsuario(id);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Buscar usuários", description = "Busca todos os usuários cadastrados.")
     @GetMapping()
     public ResponseEntity<List<DetalhePessoaResponseDTO>> buscarUsuarios() throws ServiceException {
         List<DetalhePessoaResponseDTO> response = pessoaService.buscarUsuarios();
