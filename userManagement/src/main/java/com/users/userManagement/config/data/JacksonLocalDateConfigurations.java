d74110865d63d15227968548cb153f029caa3153
anthonyMeds <anthony.eduardo@hotmail.com>
2024-12-08 23:18:41 -0300

    config: refactor to solve docker compose problem

--- userManagement/src/main/java/com/users/userManagement/config/data/JacksonLocalDateConfigurations.java
+++ userManagement/src/main/java/com/users/userManagement/config/data/JacksonLocalDateConfigurations.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.format.DateTimeFormatter;
 @Configuration
-
 public class JacksonLocalDateConfigurations {
 
     private static final String datePattern = "dd/MM/yyyy";
